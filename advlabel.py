9b590787270e39f63a93944720035b67b7cef156
ciki000 <122232105@qq.com>
2022-01-19 18:36:45 +0800

    add advlabel

--- advlabel.py
+++ advlabel.py
@@ -20,8 +20,8 @@ from utils import load_model, AverageMeter, accuracy
 
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self, transform):
-        images = np.load('./backup/data.npy')
-        labels = np.load('./backup/label.npy')
+        images = np.load('data.npy')
+        labels = np.load('label.npy')
         assert labels.min() >= 0
         assert images.dtype == np.uint8
         assert images.shape[0] <= 50000
@@ -38,6 +38,27 @@ class MyDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.labels)
 
+def adv_labels(nat_prob, y_batch, gamma=0.01):
+    #print(nat_prob, y_batch)
+    L = -np.log(nat_prob + 1e-8)  
+    LL = np.copy(L)
+    LL[np.arange(y_batch.shape[0]), y_batch] = 1e4
+    minval = np.min(LL, axis=1)
+    LL[np.arange(y_batch.shape[0]), y_batch] = -1e4
+    maxval = np.max(LL, axis=1)
+
+    num_classes = 10
+    multi = 9
+    denom = np.sum(L, axis=1) - L[np.arange(y_batch.shape[0]), y_batch] - (
+        num_classes - 1) * (minval - gamma)
+    delta = 1 / (1 + multi * (maxval - minval + gamma) / denom)
+    alpha = delta / denom
+
+    y_batch_adv = np.reshape(
+        alpha, [-1, 1]) * (L - np.reshape(minval, [-1, 1]) + gamma)
+    y_batch_adv[np.arange(y_batch.shape[0]), y_batch] = 1.0 - delta
+    #print(y_batch_adv)
+    return y_batch_adv
 
 use_cuda = torch.cuda.is_available()
 
@@ -55,14 +76,14 @@ transform_test = transforms.Compose([
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 testset = MyDataset(transform=transform_test)
-testloader = data.DataLoader(testset, batch_size=32, shuffle=False, num_workers=1)
+testloader = data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=1)
 
 # Model
 preactresnet = load_model('preactresnet18').cuda()
-preactresnet.load_state_dict(torch.load('./checkpoints/preactresnet_2.pth')['state_dict'])
+preactresnet.load_state_dict(torch.load('./checkpoints/preactresnet_4.pth')['state_dict'])
 preactresnet.eval()
 wideresnet = load_model('wideresnet').cuda()
-wideresnet.load_state_dict(torch.load('./checkpoints/wideresnet_2.pth')['state_dict'])
+wideresnet.load_state_dict(torch.load('./checkpoints/wideresnet_4.pth')['state_dict'])
 wideresnet.eval()
 
 preactresnet_accs = AverageMeter()
@@ -74,14 +95,13 @@ for (input_, soft_label) in tqdm(testloader):
     target = soft_label.argmax(dim=1)
 
     preactresnet_output = preactresnet(input_)
-    # print(target, F.softmax(preactresnet_output, dim=1))
-    # break
     wideresnet_output = wideresnet(input_)
     
-    soft_wide = F.softmax(wideresnet_output, dim=1).cpu().detach().numpy()
+    y =  torch.topk(soft_label, 1)[1].squeeze(1)
+    adv_label = adv_labels(F.softmax(wideresnet_output, dim=1).cpu().detach().numpy(), y.cpu().detach().numpy())
 
-    for i in range(soft_wide.shape[0]):
-        SoftLabels.append(soft_wide[i])
+    for i in range(adv_label.shape[0]):
+        SoftLabels.append(adv_label[i])
 
     preactresnet_acc = accuracy(preactresnet_output, target)
     wideresnet_acc = accuracy(wideresnet_output, target)
@@ -91,5 +111,5 @@ for (input_, soft_label) in tqdm(testloader):
 
 soft_labels = np.array(SoftLabels)
 print(soft_labels.shape)
-np.save('./backup/soft_label.npy', soft_labels)
+np.save('soft_label.npy', soft_labels)
 print(preactresnet_accs.avg, wideresnet_accs.avg)
